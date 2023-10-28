05adab6c91efc2099eb65d78bdd3132cc20708c0
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2023-10-28 06:43:10 -0300

    Including new componentes (initial config)

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+function Form() {
+    return (
+        <form>
+            <input type="text" placeholder="name" />
+            <input type="text" placeholder="email" />            <input type="text" placeholder="phone" />
+
+            <input type="button" value="Save" />
+            <input type="button" value="Update" />
+            <input type="button" value="Delete" />
+            <input type="button" value="Cancel" />
+        </form>
+    )
+}
+
+export default Form;
---
+++ src/Table.js
@@ -0,0 +1,26 @@
+function Table() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Select</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
